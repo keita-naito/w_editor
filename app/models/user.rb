b7f115995890c4127007d6ec26bb294452e0559d
keita-naito <atiek_nn_xx@yahoo.co.jp>
2020-07-02 19:26:08 +0900

    マイグレーション実行時のエラーを解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
