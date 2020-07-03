429e800df91dd5bf9a8e56ba1878ce967cc7fa63
keita-naito <atiek_nn_xx@yahoo.co.jp>
2020-07-03 18:11:29 +0900

    記事作成機能とテストを実装

--- spec/requests/api/v1/articles_request_spec.rb
+++ spec/requests/api/v1/articles_request_spec.rb
@@ -23,15 +23,14 @@ RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /users/:id" do
     subject { get(api_v1_article_path(article_id)) }
 
-    context "指定した id のユーザーが存在する場合" do
+    context "指定した id の記事が存在する場合" do
       let(:article) { create(:article) }
       let(:article_id) { article.id }
 
-      it "任意のユーザーの値が取得できる" do
+      it "任意の記事が取得できる" do
         subject
 
         res = JSON.parse(response.body)
-
         aggregate_failures do
           expect(response).to have_http_status(:ok)
           expect(res["id"]).to eq article.id
@@ -43,11 +42,30 @@ RSpec.describe "Api::V1::Articles", type: :request do
       end
     end
 
-    context "指定した id のユーザーが存在しない場合" do
+    context "指定した id の記事が存在しない場合" do
       let(:article_id) { 10000 }
 
-      it "ユーザーが見つからない" do
+      it "記事が見つからない" do
         expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+
+  describe "POST /articles" do
+    subject { post(api_v1_articles_path, params: params) }
+
+    let(:params) { { article: attributes_for(:article) } }
+    let(:current_user) { create(:user) }
+
+    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+
+    it "記事のレコードが作成できる" do
+      aggregate_failures do
+        expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
+        res = JSON.parse(response.body)
+        expect(res["title"]).to eq params[:article][:title]
+        expect(res["body"]).to eq params[:article][:body]
+        expect(response).to have_http_status(:ok)
       end
     end
   end
