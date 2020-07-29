36dfb792dc249c2f7ad3ee03886e5426f71643df
keitanaito <atiek.nn.xx@gmail.com>
2020-07-29 12:25:23 +0900

    記事の spec ファイルを修正

--- spec/requests/api/v1/articles_request_spec.rb
+++ spec/requests/api/v1/articles_request_spec.rb
@@ -8,15 +8,12 @@ RSpec.describe "Api::V1::Articles", type: :request do
 
     it "記事の一覧が取得できる" do
       subject
-
       res = JSON.parse(response.body)
 
-      aggregate_failures do
-        expect(response).to have_http_status(:ok)
-        expect(res.length).to eq 3
-        expect(res[0].keys).to eq ["id", "title", "body", "user"]
-        expect(res[0]["user"].keys).to eq ["id", "name", "email"]
-      end
+      expect(response).to have_http_status(:ok)
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["id", "title", "body", "user"]
+      expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
   end
 
@@ -29,16 +26,14 @@ RSpec.describe "Api::V1::Articles", type: :request do
 
       it "任意の記事が取得できる" do
         subject
-
         res = JSON.parse(response.body)
-        aggregate_failures do
-          expect(response).to have_http_status(:ok)
-          expect(res["id"]).to eq article.id
-          expect(res["title"]).to eq article.title
-          expect(res["body"]).to eq article.body
-          expect(res["user"]["id"]).to eq article.user.id
-          expect(res["user"].keys).to eq ["id", "name", "email"]
-        end
+
+        expect(response).to have_http_status(:ok)
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["user"]["id"]).to eq article.user.id
+        expect(res["user"].keys).to eq ["id", "name", "email"]
       end
     end
 
@@ -52,32 +47,27 @@ RSpec.describe "Api::V1::Articles", type: :request do
   end
 
   describe "POST /articles" do
-    subject { post(api_v1_articles_path, params: params) }
+    subject { post(api_v1_articles_path, params: params, headers: headers) }
 
     let(:params) { { article: attributes_for(:article) } }
     let(:current_user) { create(:user) }
-
-    before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+    let(:headers) { current_user.create_new_auth_token }
 
     it "記事のレコードが作成できる" do
-      aggregate_failures do
-        expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
-        res = JSON.parse(response.body)
-        expect(res["title"]).to eq params[:article][:title]
-        expect(res["body"]).to eq params[:article][:body]
-        expect(response).to have_http_status(:ok)
-      end
+      expect { subject }.to change { Article.where(user_id: current_user.id).count }.by(1)
+      res = JSON.parse(response.body)
+      expect(res["title"]).to eq params[:article][:title]
+      expect(res["body"]).to eq params[:article][:body]
+      expect(response).to have_http_status(:ok)
     end
   end
 
   describe "PATCH /api/v1/articles/:id" do
-    subject { patch(api_v1_article_path(article.id), params: params) }
+    subject { patch(api_v1_article_path(article.id), params: params, headers: headers) }
 
     let(:params) { { article: attributes_for(:article) } }
     let(:current_user) { create(:user) }
-    before do
-      allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user)
-    end
+    let(:headers) { current_user.create_new_auth_token }
 
     context "自分の記事を更新するとき" do
       let(:article) { create(:article, user: current_user) }
@@ -99,12 +89,11 @@ RSpec.describe "Api::V1::Articles", type: :request do
     end
 
     describe "DELETE /articles/:id" do
-      subject { delete(api_v1_article_path(article_id)) }
+      subject { delete(api_v1_article_path(article_id), headers: headers) }
 
-      # devise_token_auth の導入が完了後に削除
       let(:current_user) { create(:user) }
       let(:article_id) { article.id }
-      before { allow_any_instance_of(Api::V1::BaseApiController).to receive(:current_user).and_return(current_user) }
+      let(:headers) { current_user.create_new_auth_token }
 
       context "自分の記事を削除しようとするとき" do
         let!(:article) { create(:article, user: current_user) }
