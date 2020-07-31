6ef1a3caf215389c3ccd847bbbc689e3aeaf120f
keitanaito <atiek.nn.xx@gmail.com>
2020-08-01 00:05:30 +0900

    ヘッダーを実装

--- app/javascript/components/layout/Header.vue
+++ app/javascript/components/layout/Header.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>へっだー</div>
+  <v-app-bar app dark color="#3085DE">
+    <router-link to="/" class="header-link">
+      <v-toolbar-title class="white--text">WonderfulEditor</v-toolbar-title>
+    </router-link>
+
+    <v-spacer></v-spacer>
+
+    <router-link to="/" class="header-link">
+      <v-btn text class="register">ユーザー登録</v-btn>
+    </router-link>
+    <router-link to="/" class="header-link">
+      <v-btn text class="login">ログイン</v-btn>
+    </router-link>
+  </v-app-bar>
 </template>
+
+<style lang="scss" scoped>
+.header-link {
+  text-decoration: none;
+}
+.register {
+  border: 2px solid #fff;
+  border-radius: 5px;
+  font-weight: bold;
+}
+.login {
+  font-weight: bold;
+}
+</style>
