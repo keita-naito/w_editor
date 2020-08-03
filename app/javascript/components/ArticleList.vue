980074426678fb6b103a09acf38c57e62d9d9cfa
keita-naito <atiek_nn_xx@yahoo.co.jp>
2020-08-03 14:14:27 +0900

    記事一覧にupdated_at を表示

--- app/javascript/components/ArticleList.vue
+++ app/javascript/components/ArticleList.vue
@@ -2,10 +2,16 @@
   <v-container class="mt-5">
     <div id="articles-container">
       <div v-for="article in articles" v-bind:key="article.id">
-        <v-card flat class="mb-5" style="margin: 0 auto;" justify-center max-width="600" >
+        <v-card flat class="mb-5 pb-7" style="margin: 0 auto;" justify-center max-width="600" >
           <v-card-title>{{article.title}}</v-card-title>
-          <v-divider class="mx-4"></v-divider>
-          <v-card-text>{{article.body}}</v-card-text>
+          <time-ago
+            class="ml-5"
+            :refresh="60"
+            :datetime="article.updated_at"
+            locale="en"
+            tooltip="right"
+            long
+          ></time-ago>
         </v-card>
       </div>
     </div>
@@ -14,7 +20,13 @@
 
 <script>
 import axios from "axios";
+import TimeAgo from 'vue2-timeago'
+
 export default {
+  components: {
+    TimeAgo,
+  },
+
   data() {
     return {
       articles: []
